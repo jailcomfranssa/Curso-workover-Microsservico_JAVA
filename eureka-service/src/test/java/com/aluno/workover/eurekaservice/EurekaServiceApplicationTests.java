6264ea7894f3c74128b3fa23851d18abedec7906
jailcomfranssa <jail_son_0@hotmail.com>
2022-11-27 13:42:10 -0300

    first commit

---
+++ eureka-service/src/test/java/com/aluno/workover/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluno.workover.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
