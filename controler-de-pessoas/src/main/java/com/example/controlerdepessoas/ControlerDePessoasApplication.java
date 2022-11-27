d96dbac8c06f8017ddd492446fc49b8ea66d4253
jailcomfranssa <jail_son_0@hotmail.com>
2022-11-27 18:23:39 -0300

    criando MS de controle pessoas

---
+++ controler-de-pessoas/src/main/java/com/example/controlerdepessoas/ControlerDePessoasApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlerdepessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlerDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlerDePessoasApplication.class, args);
+	}
+
+}
---
+++ controler-de-pessoas/src/test/java/com/example/controlerdepessoas/ControlerDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlerdepessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlerDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
